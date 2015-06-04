759907318121674df41a606c6580724fbddf4edf
nuhuxi <mark.keckeis@gmail.com>
2015-06-04 15:26:56 -0700

    what is in the delete body?

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.delete('/items/:item', jsonParser, function(req, res) {
-
   console.log("The id is: " + req.body[0]);
-  console.log("The id is: " + req.body[1]);
 
 });
 
