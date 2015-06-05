1750f6d6827ea70874bf424bfbda5bca0220c691
nuhuxi <mark.keckeis@gmail.com>
2015-06-05 11:24:46 -0700

    First commit when adding tests

---
+++ db/seed.js
@@ -0,0 +1,26 @@
+/**
+ * Created by mark07 on 6/5/15.
+ */
+var Item = require('../models/item');
+
+exports.run = function(callback, errback) {
+  Item.create({name: 'Broad beans'},
+    {name: 'Tomatoes'},
+    {name: 'Peppers'}, function(err, items) {
+      if (err) {
+        errback(err);
+        return;
+      }
+      callback(items);
+    });
+};
+
+if (require.main === module) {
+  require('./connect');
+  exports.run(function() {
+    var mongoose = require('mongoose');
+    mongoose.disconnect();
+  }, function(err) {
+    console.error(err);
+  });
+}
---
+++ test/test-server.js
@@ -0,0 +1,101 @@
+/**
+ * Created by mark07 on 6/5/15.
+ */
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+global.environment = 'test';
+var server = require('../server.js');
+var Item = require('../models/item');
+var seed = require('../db/seed');
+
+var should = chai.should();
+var app = server.app;
+
+chai.use(chaiHttp);
+
+describe('Shopping List', function() {
+  before(function(done) {
+    seed.run(function() {
+      done();
+    });
+  });
+
+  it('should list items on GET', function (done) {
+    chai.request('http://localhost:3000')
+      .get('/items')
+      .end(function (err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.have.length(3);
+        res.body[0].should.be.a('object');
+        res.body[0].should.have.property('id');
+        res.body[0].should.have.property('name');
+        res.body[0].id.should.be.a('number');
+        res.body[0].name.should.be.a('string');
+        res.body[0].name.should.equal('Broad beans');
+        res.body[1].name.should.equal('Tomatoes');
+        res.body[2].name.should.equal('Peppers');
+        done();
+      });
+  });
+
+  it('should add an item on POST', function () {
+    chai.request('http://localhost:3000')
+      .post('/items')
+      .end(function (err, res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.have.length(3);
+        res.body[0].should.be.a('object');
+        res.body[0].should.have.property('id');
+        res.body[0].should.have.property('name');
+        res.body[0].id.should.be.a('number');
+        res.body[0].name.should.be.a('string');
+
+        done();
+      });
+  });
+
+  it('should edit an item on put', function () {
+    chai.request('http://localhost:3000')
+      .put('/items')
+      .end(function (err, res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.have.length(3);
+        res.body[0].should.be.a('object');
+        res.body[0].should.have.property('id');
+        res.body[0].should.have.property('name');
+        res.body[0].id.should.be.a('number');
+        res.body[0].name.should.be.a('string');
+        done();
+      });
+  });
+
+  it('should delete an item on delete', function () {
+    chai.request('http://localhost:3000')
+      .delete('/items')
+      .end(function (err, res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.have.length(3);
+        res.body[0].should.be.a('object');
+        res.body[0].should.have.property('id');
+        res.body[0].should.have.property('name');
+        res.body[0].id.should.be.a('number');
+        res.body[0].name.should.be.a('string');
+        done();
+      });
+  });
+
+  after(function(done) {
+    Item.remove(function() {
+      done();
+    });
+  });
+});
